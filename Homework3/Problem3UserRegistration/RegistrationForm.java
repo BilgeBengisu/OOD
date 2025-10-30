923119c00488a7461a0d2bf406664ade118046e7
BilgeBengisu <76906763+BilgeBengisu@users.noreply.github.com>
2025-10-30 14:52:33 -0400

    Problem3 Java Swing implemented

---
+++ Homework3/Problem3UserRegistration/RegistrationForm.java
@@ -0,0 +1,94 @@
+// resources: 
+// https://www.geeksforgeeks.org/java/java-jframe/
+// https://docs.oracle.com/javase/tutorial/uiswing/components/frame.html
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class RegistrationForm extends JFrame {
+    public RegistrationForm() {
+        // Create the main frame
+        JFrame frame = new JFrame("Registration Form");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(800, 600);
+
+        // Create a menu bar
+        JMenuBar menuBar = new JMenuBar();
+        JMenu fileMenu = new JMenu("Registration Form");
+        JMenuItem openItem = new JMenuItem("Open");
+        JMenuItem exitItem = new JMenuItem("Exit");
+        fileMenu.add(openItem);
+        fileMenu.addSeparator();
+        fileMenu.add(exitItem);
+        menuBar.add(fileMenu);
+
+        // the main panel for the form
+        JPanel panel = new JPanel();
+
+        // components
+        JLabel userLabel = new JLabel("Username:");
+        JTextField userField = new JTextField(15);
+        JLabel passLabel = new JLabel("Password:");
+        JPasswordField passField = new JPasswordField(15);
+        JLabel emailLabel = new JLabel("Email:");
+        JTextField emailField = new JTextField(15);
+        JLabel phoneNumberLabel = new JLabel("Phone Number:");
+        JTextField phoneNumberField = new JTextField(15);
+        JButton registerButton = new JButton("Register");
+        JButton cancelButton = new JButton("Cancel");
+
+
+        // add components to the panel
+        panel.add(userLabel);
+        panel.add(userField);
+        panel.add(passLabel);
+        panel.add(passField);
+        panel.add(emailLabel);
+        panel.add(emailField);
+        panel.add(phoneNumberLabel);
+        panel.add(phoneNumberField);
+        panel.add(registerButton);
+        panel.add(cancelButton);
+
+        // add action Listener to register the user
+        // this is where the validator will be used
+        registerButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String username = userField.getText();
+                String password = new String(passField.getPassword());
+                String email = emailField.getText();
+                String phoneNumber = phoneNumberField.getText();
+                
+                // validate credentials
+            }
+        });
+
+        cancelButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.exit(0); // Close the application
+            }
+        });
+
+        // Set layout for the main frame
+        frame.setLayout(new BorderLayout());
+        frame.setJMenuBar(menuBar);
+        frame.add(panel, BorderLayout.CENTER);
+
+        frame.setVisible(true);
+    }
+
+
+    public static void main(String[] args) {
+        // test
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                new RegistrationForm();
+            }
+        });
+    }
+}
