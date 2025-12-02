5d1279abbb5f627bfa24be07f7ad84977bfa4712
BilgeBengisu <bilgebengisu@hotmail.com.tr>
2025-12-01 21:43:25 -0500

    description added to the main panel

--- FinalProject/ui/helpers/ScreenManager.java
+++ FinalProject/ui/helpers/ScreenManager.java
@@ -27,9 +27,12 @@ public class ScreenManager {
         JPanel mainPanel = new JPanel();
         mainPanel.setLayout(new GridLayout(Math.max(3, languages.size() + 1), 1));
 
-        JLabel title = new JLabel("Choose a language to practice: ", SwingConstants.CENTER);
-        title.setFont(new Font("Arial", Font.BOLD, 18));
-        mainPanel.add(title);
+        JLabel label = new JLabel("<html>"
+        + "<div style='text-align:center; font-size:10px;'>Learn phrases natives use with language flashcards!</div>"
+        + "<div style='text-align:center; font-size:18px; font-weight:bold;'>Choose a language to practice:</div>"
+        + "</html>", SwingConstants.CENTER);
+
+        mainPanel.add(label);
 
         Font buttonFont = new Font("Arial", Font.BOLD, 16);
         for (Map.Entry<String, LanguageSelectionStrategy> e : languages.entrySet()) {
